7fedc9d995c1990679712cdcde4200063772e521
Alejandro Botero <alejandro@albotero.com>
2024-11-13 14:24:37 -0500

    Pizza page route

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/pizza/001" element={<Pizza />} />
+        <Route path="/pizza/p001" element={<Pizza />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/404" element={<NotFound />} />
         <Route path="*" element={<Navigate to="/404" replace />} />
